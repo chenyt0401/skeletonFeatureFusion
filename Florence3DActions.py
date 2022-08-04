f908eb4121a350812ebf20a62e8bdb35a70447b5
chenyt0401 <63438434+chenyt0401@users.noreply.github.com>
2022-08-04 10:30:40 +0800

    Update Florence3DActions.py

--- Florence3DActions.py
+++ Florence3DActions.py
@@ -8,28 +8,28 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 import Utils
 
-filepath = './data/FLO3D/modelBest/weights.best.hdf5'  # 只保存最佳模型
+filepath = './data/FLO3D/modelBest/weights.best.hdf5'  # save the best model
 checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1,save_weights_only=False,save_best_only=True,mode='max')
 callbacks_list = [checkpoint]
 
 data = pd.read_pickle('./data/FLO3D/30/3D_coordinate_points.pkl')
 
-train_data, test_data = Utils.trainTestSpilt.traingen_test_data(data, 30)  # 划分数据集
-train_data = Utils.generate.data_gen_1(train_data, 10)  # 模拟生成不同人骨架
+train_data, test_data = Utils.trainTestSpilt.traingen_test_data(data, 30)  # Delineate the dataset
+train_data = Utils.generate.data_gen_1(train_data, 10)  # generate skeleton data
 train_data = features.getNewFeature.get_new_feature(train_data)
 
-train_data = Utils.generate.data_gen_2(train_data, 2)  # 模拟特征片段
-train_data = Utils.generate.data_gen_3(train_data, 2)  # 模拟动作幅度
+train_data = Utils.generate.data_gen_2(train_data, 2)  # generate feature data
+train_data = Utils.generate.data_gen_3(train_data, 2)  # generate feature data
 
 test_data = features.getNewFeature.get_new_feature(test_data)
 
-# 打乱数据顺序以以泛化
+
 l1 = train_data.shape[1]
 l2 = test_data.shape[1]
 frame_num = 30
 train_data = train_data.values.reshape(-1, l1*frame_num)
 test_data = test_data.values.reshape(-1, l2*frame_num)
-np.random.shuffle(np.array(train_data))  # 只对第一维进行乱序
+np.random.shuffle(np.array(train_data))  
 np.random.shuffle(np.array(test_data))
 train_data = pd.DataFrame(train_data.reshape(-1, l1))
 test_data = pd.DataFrame(test_data.reshape(-1, l2))
@@ -71,11 +71,11 @@ history = model.fit(train_data, train_label,
 time_model_end = time.time()
 time_model = time_model_end-time_model_start
 
-# 记录数据（训练过程的）
+
 resultPath = './data/FLO3D/30/data.txt'
 model = load_model(filepath)
 loss, accuracy = model.evaluate(test_data, test_label)
-Utils.recorderStr.recorderStr(resultPath, history, model_name, accuracy, time_model)  # 记录数据 resultPath, his, modelName, accuracy, timeCost
+Utils.recorderStr.recorderStr(resultPath, history, model_name, accuracy, time_model)  #  resultPath, his, modelName, accuracy, timeCost
 
 plt.plot(history.epoch, history.history.get('accuracy'))
 plt.plot(history.epoch, history.history.get('val_accuracy'))
